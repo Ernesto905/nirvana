6b20624de7b4ed32487172bcbd7421c80812de2f
Ernesto905 <ernesto90643@gmail.com>
2024-05-09 14:21:26 -0400

    Commenced setup for authentication with Jira.

---
+++ jira/authentication.py
@@ -0,0 +1,30 @@
+import os
+import secrets
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_authorization_url():
+    scopes = ['read%3Ajira-work', 'manage%3Ajira-project', 'manage%3Ajira-configuration']
+    state = secrets.token_urlsafe(16)
+
+    authorization_url = f"""https://auth.atlassian.com/authorize?audience=api.atlassian.com&client_id={os.getenv('JIRA_CLIENT_ID')}&scope={scopes[0]}%20{scopes[1]}%20{scopes[2]}&redirect_uri={os.getenv('REDIRECT_URI')}&state={state}&response_type=code&prompt=consent"""
+
+    return authorization_url, state
+
+def get_access_token(code):
+    token_url = 'https://auth.atlassian.com/oauth/token'
+    data = {
+        'grant_type': 'authorization_code',
+        'client_id': os.getenv('JIRA_CLIENT_ID'),
+        'client_secret': os.getenv('CLIENT_SECRET'),
+        'code': code,
+        'redirect_uri': os.getenv('REDIRECT_URI')
+    }
+
+    response = requests.post(token_url, data=data)
+    response.raise_for_status()
+    access_token = response.json()['access_token']
+
+    return access_token
