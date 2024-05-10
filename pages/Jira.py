631ad6e07b746247c6b23aae48edf94bb31c4d87
Ernesto905 <ernesto90643@gmail.com>
2024-05-10 09:38:59 -0400

    Jira authentication now opens up in the same original user tab.

--- pages/Jira.py
+++ pages/Jira.py
@@ -3,24 +3,22 @@ from jira.authentication import get_authorization_url, get_access_token
 
 # Check if the user has already authenticated
 if 'access_token' not in st.session_state:
-    authorization_url, state = get_authorization_url()
+    if 'auth_state' not in st.session_state or 'code' in st.query_params:
+        if 'code' in st.query_params:
+            authorization_code = st.query_params['code']
+            access_token = get_access_token(authorization_code)
 
-    auth_button = st.link_button("Authenticate Jira", authorization_url)
+            st.session_state['access_token'] = access_token
+            st.query_params.clear()
 
-    if auth_button:
-        st.session_state['auth_state'] = state
+            st.rerun()
+        else:
+            authorization_url, state = get_authorization_url()
+            st.session_state['auth_state'] = state
+            st.markdown(f'<a href="{authorization_url}" target="_self">Click here to authenticate with Jira</a>', unsafe_allow_html=True)
+    else:
+        st.warning("Redirecting now...")
 
 else:
     st.success("Successfully Authenticated")
     access_token = st.session_state['access_token']
-
-if 'code' in st.query_params:
-    authorization_code = st.query_params['code']
-    access_token = get_access_token(authorization_code)
-
-    st.session_state['access_token'] = access_token
-    st.query_params.clear()
-
-    st.rerun()
-
-
