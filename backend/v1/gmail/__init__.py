590419f4a89db6402d9ac97f758f6e23c7d50328
Nishant Dash <nishant.dash@gmail.com>
2024-05-13 16:44:23 -0400

    Moved email pagination and pdf extraction to flask

--- backend/v1/gmail/__init__.py
+++ backend/v1/gmail/__init__.py
@@ -1,4 +1,5 @@
 import os
+import re
 import json
 import base64
 import requests
@@ -97,10 +98,21 @@ def messages():
     try:
         creds = Credentials(**session['gmail_credentials'])
         service = build("gmail", "v1", credentials=creds)
+        page_id = get_page_id(service, session, max_results, page_number, query)
 
         # Get Email IDs
-        results = (service.users().messages().list(
-            userId="me", maxResults=max_results, q=query).execute())
+        results = (
+            service
+            .users()
+            .messages()
+            .list(
+                userId="me",
+                maxResults=max_results,
+                q=query,
+                pageToken=page_id,
+                includeSpamTrash=False
+            ).execute()
+        )
 
         message_ids = results.get("messages", [])
 
@@ -127,8 +139,15 @@ def messages():
                     email_data["from"] = remove_latex(header["value"])
                 if header["name"] == "Subject":
                     email_data["subject"] = remove_latex(header["value"])
+                if header["name"] == "Date":
+                    email_data["date"] = parse_date(header["value"])
 
             email_data["body"] = get_body_text(payload)
+            email_data["message_id"] = message_id
+
+            pdf_ids = []
+            get_pdf_attachment_ids(payload, pdf_ids)
+            email_data["pdf_ids"] = pdf_ids
 
             emails.append(email_data)
         return emails
@@ -136,6 +155,47 @@ def messages():
     except HttpError as error:
         print(f"An error occurred: {error}")
         return []
+
+
+def get_page_id(service, session, maxResults, page_number, query):
+    if page_number == 1:
+        return ""
+
+    email_pages = session.get("email_pages", {})
+    page_id = email_pages.get((maxResults, page_number), "")
+    if len(page_id):
+        return page_id
+
+    try:
+        counter = 1
+        prev_page_id = ""
+        while counter != page_number:
+            prev_page_id = page_id
+            page_id = email_pages.get((maxResults, counter), None)
+            if page_id:
+                counter += 1
+                continue
+
+            results = (
+                service
+                .users()
+                .messages()
+                .list(
+                    userId="me",
+                    maxResults=maxResults,
+                    q=query,
+                    pageToken=prev_page_id,
+                    includeSpamTrash=False
+                ).execute()
+            )
+
+            page_id = results.get("nextPageToken")
+            email_pages[(maxResults, counter)] = page_id
+            counter += 1
+        return page_id
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+        return ""
 
 
 def address_from_creds(creds: dict) -> str:
@@ -173,6 +233,34 @@ def get_body_text(payload: dict) -> str:
     return ""
 
 
+def get_pdf_attachment_ids(payload: dict, ids: list):
+    if payload.get("mimeType", "") == "application/pdf":
+        if payload.get("body", {}).get("size") != 0:
+            ids.append(payload["body"]["attachmentId"])
+    for part in payload.get("parts", []):
+        get_pdf_attachment_ids(part, ids)
+
+
+def get_pdf_attachment(service, message_id: str, payload: dict):
+    pdf_ids = []
+    get_pdf_attachment_ids(payload, pdf_ids)
+    for pdf_id in pdf_ids:
+        result = (
+                service
+                .users()
+                .messages()
+                .attachments()
+                .get(
+                    userId="me",
+                    messageId=message_id,
+                    id=pdf_id
+                ).execute()
+            )
+        with open(f"temp/{pdf_id[:16]}.pdf", "wb") as f:
+            f.write(base64.urlsafe_b64decode(result["data"]))
+    return [id[:16] for id in pdf_ids]
+
+
 def decode_string(data: str, charset: str = "UTF8") -> str:
     data_binary = data.encode(charset) + b"=="
     decoded_binary = base64.urlsafe_b64decode(data_binary)
@@ -181,3 +269,18 @@ def decode_string(data: str, charset: str = "UTF8") -> str:
 
 def remove_latex(data: str) -> str:
     return data.replace("$", "\\$")
+
+
+def parse_date(date: str) -> dict:
+    expression = r"^(?P<day_of_week>\w{3}),\s+(?P<day>\d{1,2})\s+(?P<month>\w+)\s+(?P<year>\d+)\s+(?P<time>.*)$"
+    search_result = re.search(expression, date)
+    if not search_result:
+        return {"day_of_week": "",
+                "day": "",
+                "month": "",
+                "year": "",
+                "time": ""}
+    result = search_result.groupdict()
+    result["day"] = int(result["day"])
+    result["year"] = int(result["year"])
+    return result
