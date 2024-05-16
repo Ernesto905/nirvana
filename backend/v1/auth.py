fa0b0c00c2d551ce4a41a63d57883d636256f6c4
Amirali Danai <danaiamirali@gmail.com>
2024-05-16 09:42:56 -0400

    Add auth decorators for Google & Jira

---
+++ backend/v1/auth.py
@@ -0,0 +1,26 @@
+"""
+Function decorator for Flask routes that require authentication.
+
+Check for valid tokens in the request headers.
+"""
+
+from functools import wraps
+from flask import request, jsonify
+
+def google_auth_required(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        token = request.headers.get('google-auth-token')
+        if not token:
+            return jsonify({"status": 401, "response": "Unauthorized"})
+        return func(*args, **kwargs)
+    return wrapper
+
+def jira_auth_required(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        token = request.headers.get('jira-auth-token')
+        if not token:
+            return jsonify({"status": 401, "response": "Unauthorized"})
+        return func(*args, **kwargs)
+    return wrapper
