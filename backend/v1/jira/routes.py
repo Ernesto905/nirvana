3f1ac11ff8ddf8ba71832d6d91d5c45a003d5582
Ernesto905 <ernesto90643@gmail.com>
2024-05-17 09:53:51 -0400

    started adding jira routes

--- backend/v1/jira/routes.py
+++ backend/v1/jira/routes.py
@@ -1,12 +1,28 @@
 from flask import Blueprint, request, jsonify
-from backend.v1.jira.service import generate_actions, execute_action
+from backend.v1.jira.service import get_all_projects, get_all_issues
+import requests 
 
 bp = Blueprint('jira', __name__, url_prefix='/jira')
 
-@bp.route('/actions', methods=['POST'])
-def actions():
-    ...
+/jira/get-projects <- Get 
 
-@bp.route('/execute', methods=['POST'])
-def execute():
-    ...
+
+@bp.route('get-projects', method=['GET'])
+def get_all_issues():
+    access_token = resquest.headers.get('Authorization')
+    cloud_id = request.headers.get('X-Cloud-ID')
+
+    if not access_token or cloud_id:
+        return jsonify({'error': 'Missing authentication token or cloud ID'}), 401
+
+    return get_all_projects(access_token, cloud_id)
+
+@bp.route('get-issues', method=['GET'])
+def get_all_issues():
+    access_token = resquest.headers.get('Authorization')
+    cloud_id = request.headers.get('X-Cloud-ID')
+
+    if not access_token or cloud_id:
+        return jsonify({'error': 'Missing authentication token or cloud ID'}), 401
+
+    return get_all_issues(access_token, cloud_id)
