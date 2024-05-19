c5f6aa1166d3605d46c4c9f3a8e6ac445869a24e
Amirali Danai <danaiamirali@gmail.com>
2024-05-19 13:46:06 -0400

    v1/jira Update /actions to return function calls directly

--- backend/v1/jira/routes.py
+++ backend/v1/jira/routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, jsonify
 from backend.v1.jira.service import generate_actions, execute_action
 from backend.v1.auth import jira_auth_required
-from backend.v1.jira import JiraClient
+from jira import JiraClient
 
 bp = Blueprint('jira', __name__, url_prefix='/jira')
 
@@ -35,13 +35,11 @@ def actions():
 def execute():
     """
     Expected Payload:
-    - email: str with the email in question
     - action: dict with the action to be executed
     - jira-cloud-id: str with the jira cloud id
     - jira-auth-token: dict with the jira auth token
     """
     data = request.get_json()
-    email = data.get('email')
     action = data.get('action')
     jira_cloud_id = data.get('jira-cloud-id')
     jira_auth_token = data.get('jira-auth-token')
@@ -52,7 +50,7 @@ def execute():
         return jsonify({"status": 401, "error": f"Jira authentic error: {e}"})
 
     try:
-        execute_action(email, action, jc)
+        execute_action(action, jc)
     except Exception as e:
         return jsonify({"status": 500, "error": f"Error executing action: {e}"})
 
--- backend/v1/jira/service.py
+++ backend/v1/jira/service.py
@@ -1,12 +1,25 @@
 from backend.v1.llm import (
-    get_jira_actions,
-    get_jira_api_call
+    generate_actions,
+    dict_to_str
 )
 
 from jira import JiraClient
 
-def generate_actions(email, client: JiraClient):
-    ...
+def generate_actions(email: str, client: JiraClient):
+    context = client.get_allowed_params()
+    context = dict_to_str(context)
 
-def execute_action(email, action, client: JiraClient):
-    ...
+    actions = generate_actions(email, context)
+
+    return actions
+
+def execute_action(action, client: JiraClient):
+    context = client.get_allowed_params()
+    context = dict_to_str(context)
+
+    try:
+        eval("client." + action)
+    except Exception as e:
+        raise Exception(f"Error executing action during eval step: {e}")
+
+    return
