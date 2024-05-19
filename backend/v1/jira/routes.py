f24b7a1aa449c8834128fe61df60ee8590292bf5
Amirali Danai <danaiamirali@gmail.com>
2024-05-19 11:10:44 -0400

    v1/jira basic routes

--- backend/v1/jira/routes.py
+++ backend/v1/jira/routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, jsonify
 from backend.v1.jira.service import generate_actions, execute_action
 from backend.v1.auth import google_auth_required, jira_auth_required
+from backend.v1.jira import JiraClient
 
 bp = Blueprint('jira', __name__, url_prefix='/jira')
 
@@ -17,7 +18,17 @@ def actions():
     email = data.get('email')
     jira_cloud_id = data.get('jira-cloud-id')
     jira_auth_token = data.get('jira-auth-token')
-    ...
+    try:
+        jc = JiraClient(jira_cloud_id, jira_auth_token)
+    except Exception as e:
+        return jsonify({"status": 401, "error": f"Jira authentic error: {e}"})
+
+    try:
+        actions = generate_actions(email, jc)
+    except Exception as e:
+        return jsonify({"status": 500, "error": f"Error generating actions: {e}"})
+
+    return jsonify({"status": 200, "actions": actions})
 
 @bp.route('/execute', methods=['POST'])
 @jira_auth_required
@@ -32,4 +43,15 @@ def execute():
     action = data.get('action')
     jira_cloud_id = data.get('jira-cloud-id')
     jira_auth_token = data.get('jira-auth-token')
-    ...
+
+    try:
+        jc = JiraClient(jira_cloud_id, jira_auth_token)
+    except Exception as e:
+        return jsonify({"status": 401, "error": f"Jira authentic error: {e}"})
+
+    try:
+        execute_action(action, jc)
+    except Exception as e:
+        return jsonify({"status": 500, "error": f"Error executing action: {e}"})
+
+    return jsonify({"status": 200, "message": "Action executed successfully"})
